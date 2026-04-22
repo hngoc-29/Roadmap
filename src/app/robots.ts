16fc19258a0d201127924dfd1dd25fb677f6785f
HNgoc <144669382+hngoc-29@users.noreply.github.com>
2026-04-22 23:13:43 +0700

    Add '_next/' to disallow list for Googlebot

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -13,7 +13,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: "Googlebot",
         allow: "/",
-        disallow: ["/api/admin/", "/builder/"],
+        disallow: ["/api/admin/", "/builder/", "_next/"2],
       },
     ],
     sitemap: `${appUrl}/sitemap.xml`,
